4c66c3adcb0c0be41e69b99cd9701285052167fb
Shyoy <86570990+Shyoy@users.noreply.github.com>
2022-12-15 23:41:05 +0200

    working crud api

--- Back/base/urls.py
+++ Back/base/urls.py
@@ -7,7 +7,8 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('', views.index),
-    path('products/', views.myProducts, name='products'),
+    path('products/', views.all_products, name='products'),
+    path('products/<pk>', views.product_detail, name='product'),
     path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
--- Back/base/views.py
+++ Back/base/views.py
@@ -6,14 +6,12 @@ from .models import Product
 from .serializers import ProductSerializer, MyTokenObtainPairSerializer
 
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import permissions
+from rest_framework import permissions, status ,request
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 
  
 
- 
- 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
@@ -21,8 +19,43 @@ class MyTokenObtainPairView(TokenObtainPairView):
 def index(req):
     return JsonResponse('hello', safe=False)
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def myProducts(req):
-    all_products = ProductSerializer(Product.objects.all(), many=True).data
-    return Response(all_products)
+@api_view(['GET','POST','DELETE'])
+# @permission_classes([IsAuthenticated])
+def all_products(req):
+    if req.method == 'GET':
+        all_products = ProductSerializer(Product.objects.all(), many=True).data
+        return Response(all_products)
+
+    elif req.method == 'POST':
+        serializer =  ProductSerializer(data=req.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
